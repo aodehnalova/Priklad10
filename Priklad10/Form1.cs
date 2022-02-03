36d4d795ffaebf52e9200137bcc8d68e3d771241
aodehnalova <aodehnalova@spst.cz>
2022-02-03 11:34:18 +0100

    součin

--- Priklad10/Form1.cs
+++ Priklad10/Form1.cs
@@ -23,6 +23,7 @@ namespace Priklad10
             int cislo2 = Convert.ToInt32(textBox2.Text);
             label1.Text = (cislo1 + cislo2).ToString();
             label2.Text = (cislo1 - cislo2).ToString();
+            label3.Text = (cislo1 * cislo2).ToString();
         }
     }
 }
